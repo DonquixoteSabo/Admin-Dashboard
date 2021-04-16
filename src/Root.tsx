c4eb996ecf7fbd7b61ab119aede56baa80f34709
Donquixote Sabo <mixboksu@gmail.com>
2021-04-16 16:42:24 +0200

    feat: :sparkles: Add Global Styles

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,12 @@
 //styled-components setup
 import { ThemeProvider } from 'styled-components';
 import { myTheme } from 'assets/styles/my-theme';
+import { GlobalStyle } from 'assets/styles/GlobalStyle';
 
 export const Root = () => {
   return (
     <ThemeProvider theme={myTheme}>
+      <GlobalStyle />
       <h1>It works pretty good</h1>
     </ThemeProvider>
   );
