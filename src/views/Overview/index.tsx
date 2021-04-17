e2cbda4822e96a8395800ae5013073645f253fd6
Donquixote Sabo <mixboksu@gmail.com>
2021-04-17 08:51:10 +0200

    feat: :sparkles: Add Cards
    Add Cards with static data. The Data format will be exactly the same in the future

--- src/views/Overview/index.tsx
+++ src/views/Overview/index.tsx
@@ -12,6 +12,7 @@ export const Wrapper = styled.div`
     padding: 20px 0;
   }
   .cards {
+    padding-top: 40px;
     display: flex;
     justify-content: space-between;
     grid-column: 1/-1;
@@ -29,6 +30,15 @@ export const Card = styled.li`
   background-color: white;
   border: 1px solid ${({ theme }) => theme.colors.gray4};
   border-radius: 20px;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  .title {
+    padding: 0;
+    color: ${({ theme }) => theme.colors.gray3};
+    font-weight: 500;
+  }
+  .value {
+    font-weight: 600;
+  }
 `;
 
 interface CardType {
@@ -64,7 +74,8 @@ export const Overview = () => {
       <ul className="cards">
         {data.map(({ title, value }) => (
           <Card key={title}>
-            {title}, {value}
+            <p className="title">{title}</p>
+            <p className="value">{value}</p>
           </Card>
         ))}
       </ul>
