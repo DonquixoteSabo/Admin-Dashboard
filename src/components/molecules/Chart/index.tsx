ef104e23592356476a0331bd111821d99228f66a
Donquixote Sabo <mixboksu@gmail.com>
2021-04-23 06:48:59 +0200

    feat: :sparkles: Fetch chart data from mocked api

--- src/components/molecules/Chart/index.tsx
+++ src/components/molecules/Chart/index.tsx
@@ -8,34 +8,14 @@ import {
 } from 'recharts';
 //styles
 import { Wrapper } from './styles';
+//types
+import { Data } from 'types/data';
 
-const data = [
-  { hour: '0', sells: 10 },
-  { hour: '1', sells: 13 },
-  { hour: '2', sells: 15 },
-  { hour: '3', sells: 11 },
-  { hour: '4', sells: 40 },
-  { hour: '5', sells: 35 },
-  { hour: '6', sells: 30 },
-  { hour: '7', sells: 27 },
-  { hour: '8', sells: 25 },
-  { hour: '9', sells: 21 },
-  { hour: '10', sells: 18 },
-  { hour: '11', sells: 21 },
-  { hour: '12', sells: 24 },
-  { hour: '13', sells: 28 },
-  { hour: '14', sells: 30 },
-  { hour: '15', sells: 31 },
-  { hour: '16', sells: 32 },
-  { hour: '17', sells: 36 },
-  { hour: '18', sells: 40 },
-  { hour: '19', sells: 42 },
-  { hour: '20', sells: 38 },
-  { hour: '21', sells: 36 },
-  { hour: '22', sells: 35 },
-];
+interface Props {
+  data: Data[];
+}
 
-export const Chart = () => {
+export const Chart = ({ data }: Props) => {
   return (
     <Wrapper>
       <LineChart width={1000} height={400} data={data}>
--- src/components/molecules/Trends/index.tsx
+++ src/components/molecules/Trends/index.tsx
@@ -1,17 +1,32 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 //components
 import { Chart } from 'components/molecules/Chart';
 import { TodaysInformation } from 'components/atoms/TodaysInformation';
 //styles
 import { Wrapper } from './styles';
+//types
+import { Data } from 'types/data';
 
 export const Trends = () => {
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios('/overview');
+      const { data } = response.data;
+      setData(data);
+    };
+    fetch();
+  }, []);
+
   return (
     <Wrapper>
       <hgroup>
         <h1>Today's trends</h1>
         <h6>as of 17 April 2021, 10:10 AM</h6>
       </hgroup>
-      <Chart />
+      {data.length > 0 ? <Chart data={data} /> : <h1>Loading...</h1>}
       <TodaysInformation />
     </Wrapper>
   );
