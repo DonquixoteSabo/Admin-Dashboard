9bee0e77ec134e7d865a8f6f30e46381c5d33dc7
Donquixote Sabo <mixboksu@gmail.com>
2021-04-24 08:42:13 +0200

    revert: :rewind: Revert previous commit

---
+++ src/mocks/data/cards.ts
@@ -0,0 +1,21 @@
+//types
+import { CardType } from 'types/Card';
+
+export const cards: CardType[] = [
+  {
+    title: 'Unresolved',
+    value: 60,
+  },
+  {
+    title: 'Overdue',
+    value: 16,
+  },
+  {
+    title: 'Open',
+    value: 43,
+  },
+  {
+    title: 'On hold',
+    value: 64,
+  },
+];
---
+++ src/mocks/route-handlers/getters.ts
@@ -0,0 +1,37 @@
+import { rest } from 'msw';
+//data
+import { data } from 'mocks/data/index';
+import { todaysData } from 'mocks/data/TodaysData';
+import { tasks } from 'mocks/data/tasks';
+import { cards } from 'mocks/data/cards';
+
+export const getters = [
+  rest.get('/data', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        data,
+      })
+    );
+  }),
+  rest.get('/todaysData', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        todaysData,
+      })
+    );
+  }),
+  rest.get('/tasks', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        tasks,
+      })
+    );
+  }),
+  rest.get('/cards', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        cards,
+      })
+    );
+  }),
+];
